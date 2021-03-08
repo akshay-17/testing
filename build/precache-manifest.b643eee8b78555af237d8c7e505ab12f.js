9bab8e65149f51bf16969fbdc6a0613a7f7b3033
AkshayKhandelwal <akshaykhandelwal06@gmail.com>
2021-03-08 18:18:46 +0530

    first commit

---
+++ build/precache-manifest.b643eee8b78555af237d8c7e505ab12f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a8c3fa82ba5c9d6d572a3dd77289ebe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "746f6bf45f147a7a1c05",
+    "url": "/static/css/main.451e0ed1.chunk.css"
+  },
+  {
+    "revision": "8d42b92a34055e108536",
+    "url": "/static/js/2.61997b1b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.61997b1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "746f6bf45f147a7a1c05",
+    "url": "/static/js/main.35107065.chunk.js"
+  },
+  {
+    "revision": "2b55614ca07f93359093",
+    "url": "/static/js/runtime-main.bfd8333c.js"
+  }
+]);
